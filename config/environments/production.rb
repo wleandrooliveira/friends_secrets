4a3b5d535a5f203a85fafdd991fb6e75676cf622
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2020-11-22 16:33:24 -0300

    adjust to deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,14 +16,16 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile = ['*.js', '*.scss', '*.css' '*.css.erb']
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -72,7 +74,9 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
 #  config.action_mailer.default_url_options = { :host => request.host_with_port }
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000' }
   config.action_mailer.delivery_method = :smtp
+  
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
@@ -97,4 +101,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
 end
