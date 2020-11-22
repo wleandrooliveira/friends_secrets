9b57d1c2b52bfe9dc7ee60a32a9e96cdc07c7b53
Wanderson Leandro de Oliveira <wleandro.oliveira@gmail.com>
2020-11-22 02:53:23 -0300

    add controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,26 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'members/create'
+
+  get 'members/destroy'
+
+  get 'members/update'
+
+  get 'campaigns/show'
+
+  get 'campaigns/index'
+
+  get 'campaigns/create'
+
+  get 'campaigns/update'
+
+  get 'campaigns/destroy'
+
+  get 'campaigns/raffle'
+
+  get 'pages/home'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
